87da7a09bb7704b917bb47dce98abde5e69969ab
hcakil <cakilhazim@gmail.com>
2022-10-26 11:13:01 +0300

    single responsibility

--- dart-design-pattern/design_patterns/bin/design_patterns.dart
+++ dart-design-pattern/design_patterns/bin/design_patterns.dart
@@ -1,5 +1,11 @@
 import 'package:design_patterns/design_patterns.dart' as design_patterns;
+import 'package:design_patterns/models/singleton.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${design_patterns.calculate()}!');
+  // print('Hello world: ${design_patterns.calculate()}!');
+
+  for (int i = 0; i < 10; i++) {
+    Singleton? newObject = Singleton.getSingleton();
+    print("$i inci bitti");
+  }
 }
---
+++ dart-design-pattern/design_patterns/lib/models/singleton.dart
@@ -0,0 +1,23 @@
+import 'package:synchronized/synchronized.dart';
+
+class Singleton {
+  static Singleton? _singleton;
+
+  Singleton() {
+    //ctor
+    print("I 'm created");
+  }
+
+  static Singleton? getSingleton() {
+    if (_singleton == null) {
+      //For multi-threaded apps not to reach at the same time. (Queue waits)
+      var lock = Lock();
+      //This is the practice of Lazy Loading
+      lock.synchronized(() {
+        _singleton = Singleton();
+        return _singleton;
+      });
+    }
+    return _singleton;
+  }
+}
